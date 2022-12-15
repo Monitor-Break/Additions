e9da2ed5d8301b8c4be61df4fdea52400d7ee4d9
AlexanderNoles <alexnoles@outlook.com>
2022-12-15 23:14:36 +0000

    Added method descriptions to CodeSuspender.cs

--- Runtime/Misc/CodeSuspender.cs
+++ Runtime/Misc/CodeSuspender.cs
@@ -11,12 +11,22 @@ public class CodeSuspender
 
     private event Notify onTrigger;
     public List<object> registeredSuspenders = new List<object>();
+    /// <summary>
+    /// Register a method to be run when the event is triggered
+    /// </summary>
+    /// <param name="Object"></param>
+    /// <param name="methodToRun"></param>
     public void Register(object Object, Notify methodToRun) 
     {
         registeredSuspenders.Add(Object);
         onTrigger += methodToRun;
     }
 
+    /// <summary>
+    /// Unregister a mathod to be run when the event is triggered
+    /// </summary>
+    /// <param name="Object"></param>
+    /// <param name="methodRegistered"></param>
     public void UnRegister(object Object, Notify methodRegistered) 
     {
         if (registeredSuspenders.Contains(Object)) 
@@ -35,6 +45,10 @@ public class CodeSuspender
 
     private bool hasBeenTriggered = false;
     private List<object> suspendersAtTimeOfTrigger = new List<object>();
+    /// <summary>
+    /// Trigger the event
+    /// </summary>
+    /// <returns></returns>
     public bool Trigger() 
     {
         if (!hasBeenTriggered) 
@@ -55,6 +69,11 @@ public class CodeSuspender
         return hasBeenTriggered;
     }
 
+    /// <summary>
+    /// Declare the object is done with its execution. When all registered objects have declared
+    /// they are done the suspender will invoke its after suspension event.
+    /// </summary>
+    /// <param name="Object"></param>
     public void Done(object Object) 
     {
         suspendersAtTimeOfTrigger.Remove(Object);
